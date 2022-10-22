b12c9ca72e510a85337f75f58486e7e911c226f0
Pratyu01234 <75439870+Pratyu01234@users.noreply.github.com>
2022-10-22 18:16:47 +0530

    Update Reverse String.java

--- Java Questions/Reverse String.java
+++ Java Questions/Reverse String.java
@@ -1,1 +1,18 @@
 // Write a Java Program to reverse a string without using String inbuilt function.
+
+import java.util.*;
+public class ReverseString {
+
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+   
+  String str = sc.nextLine();
+  String revString = "";
+
+  for (int i = str.length() - 1; i >= 0; --i) {
+   revString += str.charAt(i);
+  }
+
+  System.out.println(revString);
+ }
+}
